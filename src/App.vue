1f07b6ae07e56c58e4911f877fe8548d6dc0e84a
wuhongjing <wuhongjing88@126.com>
2019-06-13 23:09:26 +0800

    home pages build

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="header">
+      i am header
+    </div>
+    <div class="tab">
+      i am tab
+    </div>
   </div>
 </template>
 
@@ -12,12 +16,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
